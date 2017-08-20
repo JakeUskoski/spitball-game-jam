a40d90da2ea6bf1febe86d7f997ff9736839f936
Jacob Uskoski <juskoski@mail.uoguelph.ca>
2017-08-20 16:45:51 -0400

    Moved files and fixed enemies

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField] private float m_TimeLimit = 300f;
+
+	private GameObject player;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

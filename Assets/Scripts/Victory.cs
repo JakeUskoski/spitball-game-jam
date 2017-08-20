fa584de32d728e123106e8d0e172f6b48d915a3b
Jacob Uskoski <juskoski@mail.uoguelph.ca>
2017-08-20 17:54:17 -0400

    VictorySpace

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour {
+
+	private GameManager manager;
+
+	void Awake() {
+		manager = Camera.main.GetComponent<GameManager> ();
+	}
+		
+	void OnTiggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			manager.WinGame ();
+		}
+	}
+}
